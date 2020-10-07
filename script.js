6ee7d3d7e0ec4c11a26692941b576d8703507b87
muhammad awwad <hammode.awad@gmail.com>
2020-10-07 15:25:20 +0300

    AWWAD:  stage3

--- script.js
+++ script.js
@@ -4,3 +4,40 @@ const toggle = document.querySelector("#toggleNavbar");
 toggle.addEventListener("click", () => {
   menu.classList.toggle("show");
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*AWWAD:  Some Animation for members pic*/
+let usersPics=document.querySelectorAll(".users-pic");
+usersPics.forEach(img=>{
+  img.addEventListener("mouseover",function(){
+    img.classList.add("rotate-pic");
+    setTimeout(()=>img.classList.remove("rotate-pic"),0.4*1000)
+  })
+})
+
+/*AWWAD:  Some Animation for members seection*/
+let membersSec=document.querySelector(".users-info");
+membersSec.addEventListener("mouseenter",function(){
+  usersPics.forEach(img=>{
+      img.classList.add("rotate-pic");
+      setTimeout(()=>img.classList.remove("rotate-pic"),0.4*1000)
+  
+  })
+});
