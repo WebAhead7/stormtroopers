22d9f196d6e79de8d69e22cb294cd4a68dc7f8b8
muhammad awwad <hammode.awad@gmail.com>
2020-10-07 17:00:54 +0300

    AWWAD: LastStage

--- script.js
+++ script.js
@@ -5,15 +5,9 @@ toggle.addEventListener("click", () => {
   menu.classList.toggle("show");
 });
 
-/*AWWAD:  Some Animation for members pic*/
-let usersPics = document.querySelectorAll(".users-pic");
-usersPics.forEach((img) => {
-  img.addEventListener("mouseover", function () {
-    img.classList.add("rotate-pic");
-    setTimeout(() => img.classList.remove("rotate-pic"), 0.4 * 1000);
-  });
-});
 
+
+let usersPics = document.querySelectorAll(".users-pic");
 /*AWWAD:  Some Animation for members seection*/
 let membersSec = document.querySelector(".users-info");
 membersSec.addEventListener("mouseenter", function () {
